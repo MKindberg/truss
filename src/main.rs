01e4519f483a7820306c02d5e392155d748084b0
Marcus <6180175+MKindberg@users.noreply.github.com>
2024-01-15 21:56:57 +0100

    Start parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod rss;
+mod args;
+
+use clap::Parser;
 
 fn main() -> std::io::Result<()> {
+    let args = args::Args::parse();
     let channel = rss::Channel::new("sample-rss-2.xml").expect("Failed to parse xml");
-
-    dbg!(channel);
+    // dbg!(channel);
     Ok(())
 }
