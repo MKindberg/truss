a27d7a6a70a808c618b6b9342962f4bd0bbe5a98
Marcus <6180175+MKindberg@users.noreply.github.com>
2024-01-20 21:44:40 +0100

    Read feeds from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod args;
-mod rss;
 mod picker;
+mod rss;
 
 use clap::Parser;
 use std::io::BufRead;
@@ -11,15 +11,26 @@ fn main() -> std::io::Result<()> {
     if let Some(filename) = args.filename {
         let file = File::open(filename)?;
         let reader = BufReader::new(file);
-        let mut channels = Vec::new();
-        for url in reader.lines() {
-            let resp = reqwest::blocking::get(url.unwrap()).unwrap();
-            let file = BufReader::new(resp);
-            let channel = rss::Channel::new(file).expect("Failed to parse xml");
-            channels.push(channel);
-        }
+        let channels = parse_channels(reader.lines());
+
+        let channel = picker::select(&channels);
+        let _item = picker::select(&channel.items);
+    } else {
+        let channels = parse_channels(std::io::stdin().lock().lines());
+
         let channel = picker::select(&channels);
         let _item = picker::select(&channel.items);
     }
     Ok(())
+}
+
+fn parse_channels<B: BufRead>(lines: std::io::Lines<B>) -> Vec<rss::Channel> {
+    let mut channels = Vec::new();
+    for url in lines {
+        let resp = reqwest::blocking::get(url.unwrap()).unwrap();
+        let file = BufReader::new(resp);
+        let channel = rss::Channel::new(file).expect("Failed to parse xml");
+        channels.push(channel);
+    }
+    channels
 }
